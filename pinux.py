ab44e2c2519afa976c1c68ea22a92479ea041e7a
Kyberspace <89725290+Dorabotmaster@users.noreply.github.com>
2022-01-02 19:17:42 +0530

    Create pinux.py

---
+++ pinux.py
@@ -0,0 +1,109 @@
+import os
+
+os.system('clear')
+
+import colorama
+
+from colorama import Fore
+
+print(Fore.RED + '[1]Linux')                                                                                                                           print(Fore.RED + '[2]Andrax')
+
+print(Fore.RED + '[3]Tools')
+
+print(Fore.RED + '[4]Top 10')
+
+print(Fore.RED + '[0]exit')
+
+answer1 = input("Choose an option>")
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[1]ALPINE')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[2]Backbox')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[3]CENTOS')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[4]Debian')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[5]Fedora')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[6]kali')
+
+if answer1 == "1":                                                                                                                                      print(Fore.GREEN + '[7]parrot')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[8]Ubuntu')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[9]Termux-desktop')
+
+if answer1 == "1":
+
+ print(Fore.GREEN + '[0]exit')
+
+answer2 = input("Choose os>")
+
+if answer2 == "1":
+
+ os.system('pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/In>
+
+if answer2 == "2":
+
+ os.system('pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Instal>
+
+if answer2 == "3":
+
+ os.system('pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/In>
+
+if answer2 == "4":
+
+ os.system('pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Instal>
+
+if answer2 == "5":
+
+ os.system('pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/In>
+
+if answer2 == "6":
+
+ os.system('pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Instal>
+
+if answer2 == "7":
+
+ os.system('pkg install wget openssl-tool proot -y && hash -r && wget ')
+
+if answer2 == "9":
+
+ os.system('')
+
+if answer2 == "8":
+
+ os.system('apt install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Instal>
+
+if answer2 == "9":
+
+ os.system('git clone --depth=1 https://github.com/adi1090x/termux-desktop.git')
+
+if answer2 == "9":
+
+ os.system('cd termux-desktop')
+
+if answer2 == "9":
+
+ os.system('chmod +x setup.sh')
+
+if answer2 == "9":
+
+ os.system('./setup.sh --install')
